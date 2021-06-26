d89a7975a9fb15f900403013364dc0d04e606a56
vladlen <vladlengg@gmail.com>
2021-06-26 20:01:03 +0500

    update input-image admin add accepnt

--- client/admin/src/lib/components/form/input-image/InputImage.tsx
+++ client/admin/src/lib/components/form/input-image/InputImage.tsx
@@ -1,29 +1,36 @@
 import React, {useEffect, useState} from "react"
 import "./InputImage.less"
-import {Form, FormInstance} from "antd"
+import {Form, FormInstance, message} from "antd"
 import {Rule} from "rc-field-form/lib/interface"
 import {PlusOutlined, EditOutlined} from "@ant-design/icons"
 import {getBase64} from "../../../../utils/getBase64"
 
 interface InputImageProps {
+    accept?: string
     name: string
     form: FormInstance
     rules?: Rule[]
 }
 
-const InputImage: React.FC<InputImageProps> = ({name, form, rules}) => {
+const InputImage: React.FC<InputImageProps> = ({
+    name,
+    form,
+    rules,
+    accept = "image/x-png,image/jpeg,image/png"
+}) => {
     const [image, setImage] = useState<any>(form.getFieldValue(name) || null)
 
     const addInputChangeHandler = (e: any) => {
         if (e.target.files.length) {
             // Get this url from response in real world.
-            return Object.values(e.target.files).map((file: any) =>
-                getBase64(file, (imageUrl: any) => {
-                    setImage(imageUrl)
-                })
-            )
-        }
-        else setImage(null)
+            return Object.values(e.target.files).map((file: any) => {
+                if (accept.includes(file.type))
+                    getBase64(file, (imageUrl: any) => {
+                        setImage(imageUrl)
+                    })
+                else message.error("Данный формат изображения не поддерживается!")
+            })
+        } else setImage(null)
     }
 
     useEffect(() => {
@@ -36,7 +43,7 @@ const InputImage: React.FC<InputImageProps> = ({name, form, rules}) => {
                 {image ? (
                     <label className="output-image">
                         <img src={image} alt="" />
-                        <input type="file" onChange={addInputChangeHandler} />
+                        <input type="file" onChange={addInputChangeHandler} accept={accept} />
                         <div className="edit-image">
                             <EditOutlined />
                         </div>
@@ -44,7 +51,7 @@ const InputImage: React.FC<InputImageProps> = ({name, form, rules}) => {
                 ) : (
                     <label className="input-image-add">
                         <PlusOutlined />
-                        <input type="file" onChange={addInputChangeHandler} />
+                        <input type="file" onChange={addInputChangeHandler} accept={accept} />
                     </label>
                 )}
             </div>

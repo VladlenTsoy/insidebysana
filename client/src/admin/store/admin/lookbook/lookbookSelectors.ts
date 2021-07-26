18491b62d62646d54eb7a9e38266c018431bfd26
vladlen <vladlengg@gmail.com>
2021-07-26 16:27:21 +0500

    fix lookbook output

--- client/src/admin/store/admin/lookbook/lookbookSelectors.ts
+++ client/src/admin/store/admin/lookbook/lookbookSelectors.ts
@@ -1,13 +1,17 @@
 import {useSelector} from "react-redux"
 import {AdminState} from "../store"
-import {selectAllLookbook} from "./lookbookSlice"
+// import {selectAllLookbook} from "./lookbookSlice"
 import {selectAll} from "./lookbookCategorySlice"
 
 // Загрузка
 export const useLoadingLookbook = () => useSelector((state: AdminState) => state.lookbook.loading)
 
 // Вывод всех
-export const useSelectAllLookbook = () => useSelector(selectAllLookbook)
+// export const useSelectAllLookbook = () => useSelector(selectAllLookbook)
+export const useSelectLookbookByCategoryId = (categoryId: number) =>
+    useSelector((state: AdminState) =>
+        Object.values(state.lookbook.entities).filter(lookbook => lookbook?.category_id === categoryId)
+    )
 
 // Загрузка
 export const useLoadingLookbookCategory = () =>

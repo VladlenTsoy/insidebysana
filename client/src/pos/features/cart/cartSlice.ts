8cbaaa5916b4158b2dc48e57f5859bcf0a2cf552
vladlen <vladlengg@gmail.com>
2021-07-30 04:07:42 +0500

    update discount format

--- client/src/pos/features/cart/cartSlice.ts
+++ client/src/pos/features/cart/cartSlice.ts
@@ -4,6 +4,7 @@ import {StoreState} from "pos/store"
 import {fetchProductColorBySKU} from "./fetchProductColorBySKU"
 import {AdditionalService} from "types/AdditionalService"
 import {useSelector} from "react-redux"
+import {updateTotal} from "./cartUpdateTotal"
 
 export const cartAdapter = createEntityAdapter<CartProductItemType>({
     selectId: product => `${product.product_color_id}${product.size_id}`
@@ -26,9 +27,9 @@ export interface StateProps {
         label: string
         price: number
     }[]
-    payChange: number
-    leftToPay: number
-    processing: boolean
+    payChange: number // Сдачи
+    leftToPay: number // Осталось оплатить
+    processing: boolean // На обработку
 }
 
 const initialState = cartAdapter.getInitialState<StateProps>({
@@ -40,40 +41,6 @@ const initialState = cartAdapter.getInitialState<StateProps>({
     leftToPay: 0,
     processing: false
 })
-
-// Обновить итоговую стоимость
-const updateTotal = (state: StoreState["cart"]) => {
-    const {discount, addtionalServices} = state
-    const products = Object.values(state.entities)
-    let totalPrice = 0
-
-    if (products.length) {
-        totalPrice += products.reduce((acc, {product, qty}: any) => {
-            const price = product.discount
-                ? (product.price - (product.price / 100) * product.discount).toFixed(0)
-                : product.price
-            return acc + Number(price) * qty
-        }, 0)
-    }
-
-    if (discount && totalPrice > 0) {
-        if (discount.type === "percent") totalPrice = totalPrice - (totalPrice / 100) * discount.discount
-        else totalPrice = totalPrice - discount.discount
-    }
-
-    if (addtionalServices.length)
-        totalPrice += addtionalServices.reduce(
-            (acc, addtionalService) => (acc += addtionalService.price * addtionalService.qty),
-            0
-        )
-
-    const totalPricePayments = state.payments.reduce((acc, payment) => (acc += payment.price), 0)
-
-    // Осталось оплатить
-    const leftToPay = totalPrice - totalPricePayments
-
-    return [totalPrice, leftToPay]
-}
 
 const cartSlice = createSlice({
     name: "cart",
---
+++ client/src/pos/features/cart/cartUpdateTotal.ts
@@ -0,0 +1,38 @@
+import {StoreState} from "pos/store"
+
+// Обновить итоговую стоимость
+export const updateTotal = (state: StoreState["cart"]) => {
+    const {discount, addtionalServices} = state
+    const products = Object.values(state.entities)
+    let totalPrice = 0
+
+    // Добавление товаров
+    if (products.length) {
+        totalPrice += products.reduce((acc, {product, qty}: any) => {
+            const price = product.discount.discount
+                ? (product.price - (product.price / 100) * product.discount.discount).toFixed(0)
+                : product.price
+            return acc + Number(price) * qty
+        }, 0)
+    }
+
+    // Скидка
+    if (discount && totalPrice > 0) {
+        if (discount.type === "percent") totalPrice = totalPrice - (totalPrice / 100) * discount.discount
+        else totalPrice = totalPrice - discount.discount
+    }
+
+    // Добавление допб услуг
+    if (addtionalServices.length)
+        totalPrice += addtionalServices.reduce(
+            (acc, addtionalService) => (acc += addtionalService.price * addtionalService.qty),
+            0
+        )
+
+    // Общая введенная сумма
+    const totalPricePayments = state.payments.reduce((acc, payment) => (acc += payment.price), 0)
+    // Осталось оплатить
+    const leftToPay = totalPrice - totalPricePayments
+
+    return [totalPrice, leftToPay]
+}

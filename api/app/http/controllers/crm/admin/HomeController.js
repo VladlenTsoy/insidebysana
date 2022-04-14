7d3e10fff20f973af31e30ca612d20c264ee390e
vladlengg <ocvbjf5b24aq4snhfnbtlgzuvf4s6vdeygk72cjuxxap3s46o6vq>
2022-04-15 01:47:01 +0500

    add position

--- api/app/http/controllers/crm/admin/HomeController.js
+++ api/app/http/controllers/crm/admin/HomeController.js
@@ -1,108 +1,120 @@
 const {Order} = require("models/orders/Order")
+const {OrderProductColor} = require("models/orders/OrderProductColor")
 const {logger} = require("config/logger.config")
 const {Client} = require("models/Client")
 const moment = require("moment")
 const {raw} = require("objection")
 
 const GetStatistic = async (req, res) => {
-    const {dateFrom, dateTo} = req.body
-    const response = {
-        revenue: 0,
-        costs: 0,
-        numberOfChecks: 0,
-        numberOfPositions: 0,
-        numberOfOnlineOrders: 0,
-        numberOfNewClients: 0,
-        averageCheck: 0,
-        revenueByDay: []
-    }
-
-    const revenue = Order.query()
-        .where(builder => {
-            builder.whereNull("status_id")
-                .orWhere("status_id", 16)
-        }).sum({total: "total_price"})
-    if (dateFrom && dateTo)
-        revenue.whereBetween("created_at", [
-            moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
-            moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
-        ])
-
-    response.revenue = (await revenue)[0]?.total || 0
-
-
-    const numberOfChecks = Order.query()
-        .where(builder => {
-            builder.whereNull("status_id")
-                .orWhere("status_id", 16)
-        }).count("*", {as: "total"})
-        .whereIn("source_id", [5, 6])
-    if (dateFrom && dateTo)
-        numberOfChecks.whereBetween("created_at", [
-            moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
-            moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
-        ])
-
-    response.numberOfChecks = (await numberOfChecks)[0]?.total || 0
-
-
-    const numberOfOnlineOrders = Order.query()
-        .where(builder => {
-            builder.whereNull("status_id")
-                .orWhere("status_id", 16)
-        }).count("*", {as: "total"})
-        .whereNotIn("source_id", [5, 6])
-    if (dateFrom && dateTo)
-        numberOfOnlineOrders.whereBetween("created_at", [
-            moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
-            moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
-        ])
-
-    response.numberOfOnlineOrders = (await numberOfOnlineOrders)[0]?.total || 0
-
-
-    const averageCheck = Order.query()
-        .where(builder => {
-            builder.whereNull("status_id")
-                .orWhere("status_id", 16)
-        }).avg({total: "total_price"})
-    if (dateFrom && dateTo)
-        averageCheck.whereBetween("created_at", [
-            moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
-            moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
-        ])
-
-    response.averageCheck = (await averageCheck)[0]?.total || 0
-
-
-    const numberOfNewClients = Client.query()
-        .count("*", {as: "total"})
-    if (dateFrom && dateTo)
-        numberOfNewClients.whereBetween("created_at", [
-            moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
-            moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
-        ])
-
-    response.numberOfNewClients = (await numberOfNewClients)[0]?.total || 0
-
-
-    const revenueByDay = Order.query()
-        .select(raw(`DATE(created_at) AS date`))
-        .sum({total: "total_price"})
-        .groupBy("date")
-        .where(builder => {
-            builder.whereNull("status_id")
-                .orWhere("status_id", 16)
-        })
-    if (dateFrom && dateTo)
-        revenueByDay.whereBetween("created_at", [
-            moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
-            moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
-        ])
-
-    response.revenueByDay = await revenueByDay
-
     try {
+        const {dateFrom, dateTo} = req.body
+        const response = {
+            revenue: 0,
+            costs: 0,
+            numberOfChecks: 0,
+            numberOfPositions: 0,
+            numberOfOnlineOrders: 0,
+            numberOfNewClients: 0,
+            averageCheck: 0,
+            revenueByDay: []
+        }
+
+        const revenue = Order.query()
+            .where(builder => {
+                builder.whereNull("status_id")
+                    .orWhere("status_id", 16)
+            }).sum({total: "total_price"})
+        if (dateFrom && dateTo)
+            revenue.whereBetween("created_at", [
+                moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
+                moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
+            ])
+
+        response.revenue = (await revenue)[0]?.total || 0
+
+        const numberOfChecks = Order.query()
+            .where(builder => {
+                builder.whereNull("status_id")
+                    .orWhere("status_id", 16)
+            }).count("*", {as: "total"})
+            .whereIn("source_id", [5, 6])
+        if (dateFrom && dateTo)
+            numberOfChecks.whereBetween("created_at", [
+                moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
+                moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
+            ])
+
+        response.numberOfChecks = (await numberOfChecks)[0]?.total || 0
+
+
+        const numberOfOnlineOrders = Order.query()
+            .where(builder => {
+                builder.whereNull("status_id")
+                    .orWhere("status_id", 16)
+            }).count("*", {as: "total"})
+            .whereNotIn("source_id", [5, 6])
+        if (dateFrom && dateTo)
+            numberOfOnlineOrders.whereBetween("created_at", [
+                moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
+                moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
+            ])
+
+        response.numberOfOnlineOrders = (await numberOfOnlineOrders)[0]?.total || 0
+
+
+        const averageCheck = Order.query()
+            .where(builder => {
+                builder.whereNull("status_id")
+                    .orWhere("status_id", 16)
+            }).avg({total: "total_price"})
+        if (dateFrom && dateTo)
+            averageCheck.whereBetween("created_at", [
+                moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
+                moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
+            ])
+
+        response.averageCheck = (await averageCheck)[0]?.total || 0
+
+
+        const numberOfNewClients = Client.query()
+            .count("*", {as: "total"})
+        if (dateFrom && dateTo)
+            numberOfNewClients.whereBetween("created_at", [
+                moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
+                moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
+            ])
+
+        response.numberOfNewClients = (await numberOfNewClients)[0]?.total || 0
+
+
+        const revenueByDay = Order.query()
+            .select(raw(`DATE(created_at) AS date`))
+            .sum({total: "total_price"})
+            .groupBy("date")
+            .where(builder => {
+                builder.whereNull("status_id")
+                    .orWhere("status_id", 16)
+            })
+        if (dateFrom && dateTo)
+            revenueByDay.whereBetween("created_at", [
+                moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
+                moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
+            ])
+
+        response.revenueByDay = await revenueByDay
+
+
+        const numberOfPositions = OrderProductColor.query()
+            .count("*", {as: "total"})
+
+        if (dateFrom && dateTo)
+            numberOfPositions.whereBetween("created_at", [
+                moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
+                moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
+            ])
+
+        response.numberOfPositions = (await numberOfPositions)[0]?.total || 0
+
         res.send(response)
     } catch (e) {
         logger.error(e.stack)
